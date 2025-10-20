707d9e9f1dbf69747af10aef9a27cb1dd7051fb6
paschini <camila.paschini@gmail.com>
2025-10-20 16:06:48 +0200

    end video For Iteration

---
+++ ForIteration/Program.cs
@@ -0,0 +1,36 @@
+﻿// Created by following this video:
+// [x] https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/for-iteration-statement
+
+// not required anymore...
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found Seven!");
+                    break;
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue); 
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
+
