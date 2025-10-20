d16b13c24361603f91a09a7f2a9f461a1a23edf7
paschini <camila.paschini@gmail.com>
2025-10-20 14:37:22 +0200

    Hello World!

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// Created by following this video: https://learn.microsoft.com/en-us/shows/csharp-fundamentals-for-absolute-beginners/creating-your-first-c-program
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
