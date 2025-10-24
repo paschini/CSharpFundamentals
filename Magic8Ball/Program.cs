edf9523af9a342618f02858b9ef485c2081008a3
paschini <camila.paschini@gmail.com>
2025-10-24 10:53:19 +0200

    tweak to show my opinion xD

--- Magic8Ball/Program.cs
+++ Magic8Ball/Program.cs
@@ -12,11 +12,12 @@
 using Magic8Ball;
 
 Console.WriteLine();
-foreach(var s in args)
+foreach (var s in args)
 {
     Console.Write(s);
     Console.Write(' ');
 }
+
 Console.WriteLine();
 
 // var utils = new Utilities();
@@ -27,17 +28,38 @@ await Utilities.ShowConsoleAnimation();
 
 string[] answers =
 [
-    "It is certain.",       "Reply hazy, try again.",     "Don't count on it.",
-    "It is decidedly so.",  "Ask again later.",           "My reply is no.",
-    "Without a doubt.",     "Better not tell you now.",   "My sources say no.",
-    "Yes – definitely.",    "Cannot predict now.",        "Outlook not so good.",
-    "You may rely on it.",  "Concentrate and ask again.", "Very doubtful.",
+    "It is certain.", "Reply hazy, try again.", "Don't count on it.",
+    "It is decidedly so.", "Ask again later.", "My reply is no.",
+    "Without a doubt.", "Better not tell you now.", "My sources say no.",
+    "Yes – definitely.", "Cannot predict now.", "Outlook not so good.",
+    "You may rely on it.", "Concentrate and ask again.", "Very doubtful.",
     "As I see it, yes.",
     "Most likely.",
     "Outlook good.",
     "Yes.",
     "Signs point to yes.",
 ];
+
+string[] onlyNegatives =
+[
+    "Don't count on it.",
+    "My reply is no.",
+    "Better not tell you now.",
+    "My sources say no.",
+    "Cannot predict now.",
+    "Outlook not so good.",
+    "Very doubtful.",
+    "No!",
+    "Nej!",
+    "Niet!"
+];
+
+if (args[0] == "ask" && args.Length > 1)
+{
+    // I'm not sure I like top level statements... Brain too rotted in C?
+    var negativeIndex = new Random().Next(answers.Length - 1);
+    Console.WriteLine(onlyNegatives[negativeIndex]);
+}
 
 var index = new Random().Next(answers.Length - 1);
 Console.WriteLine(answers[index]);
